73f27ea612ef6957264d7d3182db244cd04e71b6
Rong Sen Ng <wes.ngrongsen@gmail.com>
2018-04-15 20:57:09 +0800

    💥 Now uses `parse5` to render content 🐛 Various bug fixes

--- src/demo/index.js
+++ src/demo/index.js
@@ -1,13 +1,12 @@
 // @ts-check
 
 import restify from 'restify';
-import { ntml } from 'lit-ntml';
+import parse5 from 'parse5';
 
 import { formila } from '../../dist';
 // @ts-ignore
 import formOpts from './json/form.json';
 
-const html = ntml({ parseHtml: true, minify: false });
 const server = restify.createServer();
 
 server.use(restify.plugins.bodyParser({
@@ -21,59 +20,57 @@ server.get('/healthcheck', async (_, res) => {
 });
 server.get('/demo', async (_, res, next) => {
   const d = await formila(formOpts);
+  const rendered = parse5.serialize(parse5.parse(`<body>
+  <style>
+    html,
+    body {
+      width: 100%;
+      min-height: 100vh;
+      margin: 0;
+      padding: 0;
+      background-color: #0070fb;
+      color: #000;
+      font-size: 16px;
+      font-family: sans-serif;
+      box-sizing;
+    }
 
-  console.log(d.html);
+    * {
+      box-sizing: border-box;
+    }
+
+    body {
+      background-color: #fff;
+    }
+
+    main {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+
+      width: 100%;
+      min-height: 100vh;
+    }
+
+    form {
+      max-width: calc(100% / 16 * 9);
+      width: auto;
+      margin: 0 auto;
+      padding: 16px 24px;
+      box-shadow: 0 8px 10px 1px rgba(0, 0, 0, 0.14),
+                  0 3px 14px 2px rgba(0, 0, 0, 0.12),
+                  0 5px 5px -3px rgba(0, 0, 0, 0.4);
+    }
+  </style>
+  ${d.style}
+  <main>${d.html}</main>
+</body>`));
 
   res.writeHead(200, {
     'content-type': 'text/html',
   });
-  res.end(await html`<body>
-    <style>
-      html,
-      body {
-        width: 100%;
-        min-height: 100vh;
-        margin: 0;
-        padding: 0;
-        background-color: #0070fb;
-        color: #000;
-        font-size: 16px;
-        font-family: sans-serif;
-        box-sizing;
-      }
-
-      * {
-        box-sizing: border-box;
-      }
-
-      body {
-        background-color: #fff;
-      }
-
-      main {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-
-        width: 100%;
-        min-height: 100vh;
-      }
-
-      form {
-        max-width: calc(100% / 16 * 9);
-        width: auto;
-        margin: 0 auto;
-        padding: 16px 24px;
-        box-shadow: 0 8px 10px 1px rgba(0, 0, 0, 0.14),
-                    0 3px 14px 2px rgba(0, 0, 0, 0.12),
-                    0 5px 5px -3px rgba(0, 0, 0, 0.4);
-      }
-    </style>
-    ${d.style}
-    <main>${d.html}</main>
-  </body>`);
-  return next();
+  return res.end(rendered);
 });
 server.post('/demo', (req, res) => {
   const uploadedFiles = Object.keys(req.files)
--- src/index.ts
+++ src/index.ts
@@ -26,19 +26,74 @@ export declare interface FormilaOpts {
 export type Omit<T, U> = Pick<T, Exclude<keyof T, U>>;
 
 /** Import project dependencies */
-import { ntml } from 'lit-ntml';
+import parse5 from 'parse5';
 
 /** Setting up */
-const html = ntml({
-  parseHtml: false,
-  minify: false,
-});
 export const ATTR_ID_REGEX = /.+id\=\"(.+?)\".*/i;
+
+function parseFieldTagFragment(fieldTag: string) {
+  const parsedFragment = parseFragment(fieldTag);
+  const newParsedFragment = {
+    ...parsedFragment,
+    childNodes: parsedFragment.childNodes.map((n) => {
+      if (!Array.isArray(n.attrs) || !n.attrs.length) {
+        return n;
+      }
+
+      const attrsMap = new Map([
+        ...n.attrs.map(nn => [nn.name, nn.value]),
+        ['aria-invalid', 'false'],
+        ['aria-required', 'false'],
+      ]);
+
+      if (attrsMap.has('required')) {
+        attrsMap.set('aria-required', 'true');
+      }
+
+      return {
+        ...n,
+        attrs: [...attrsMap].map((nn) => {
+          return { name: nn[0], value: nn[1] };
+        }),
+      };
+    }),
+  };
+
+  return parse5.serialize(newParsedFragment);
+}
+
+function parseAttrs(attr: string) {
+  const { childNodes } = parseFragment(`<div ${attr}></div>`);
+  const { attrs } = childNodes.find(n => n.nodeName === 'div');
+
+  return Array.isArray(attrs) && attrs.length > 0
+    ? ` ${attrs.map(n => `${n.name}="${n.value}"`).join(' ')}`
+    : '';
+}
+
+function getIdFromFieldTag(fieldTag: string) {
+  const { childNodes } = parseFragment(fieldTag);
+  const { attrs } = childNodes.find(n => /(input|select)/i.test(n.nodeName));
+  const { value } = attrs.find(n => /id/i.test(n.name));
+  const htmlFor = attrs.find(n => /for/i.test(n.name));
+
+  return htmlFor == null
+    ? ` for="${value}"`
+    : '';
+}
+
+function serializeParsedFragment(content: string) {
+  return parse5.serialize(parseFragment(content));
+}
+
+function parseFragment(content: string) {
+  return parse5.parseFragment(content);
+}
 
 function appendLabel(
   isPrefixed: FormilaOptsFieldsetField['isPrefixed']
 ) {
-  return async function appendInput(
+  return function appendInput(
     data: Omit<FormilaOptsFieldsetField, 'isPrefixed'>
   ) {
     const {
@@ -53,273 +108,241 @@ function appendLabel(
       throw new Error('Missing required attribute \'id\' in opts[fieldTag]');
     }
 
-    return html`<div class="label-container">
+    return `<div class="label-container">
     <label${
-      attrTag == null
-        ? ` for="${fieldTag.replace(ATTR_ID_REGEX, '$1')}"`
-        : ` ${await html`${attrTag}${
-          ATTR_ID_REGEX.test(fieldTag)
-            ? ''
-            : ` for="${fieldTag.replace(ATTR_ID_REGEX, '$1')}"`
-        }`}`
+      parseAttrs(`${getIdFromFieldTag(fieldTag)} ${
+        attrTag == null || (typeof attrTag === 'string' && !attrTag.length)
+          ? ''
+          : attrTag
+      }`)
     }><div class="input-title">${
       title
     }</div>${
       typeof isPrefixed !== 'boolean' || !isPrefixed
         ? ''
-        : html`<div class="prefixed-input${
-          description == null
-            ? ''
-            : ' has-description'
-        }">`
+        : `<div class="prefixed-input${description == null ? '' : ' has-description'}">`
     }${
       fieldTag == null
         ? ''
-        : html`${fieldTag.replace(
-          /(>.*?<\/(input|select)>.(.*))/gi,
-          ` aria-required="${
-            !/(required=\"false\")/i.test(fieldTag)
-              && /(required\=\"true\"|required\s*)/i.test(fieldTag)
-                ? 'true'
-                : 'false'
-          }" aria-invalid="false"$1`
-        )}`
+        : parseFieldTagFragment(fieldTag)
     }
     ${
       description == null
         ? ''
-        : html`<div class="input-description">${description}</div>`
+        : `<div class="input-description">${description}</div>`
     }
     ${
       isPrefixed ? '</div>' : ''
     }${
       errorMessage == null
         ? ''
-        : html`<div class="error-msg"
-          role="alert">${errorMessage}</div>`
+        : `<div class="error-msg" role="alert">${errorMessage}</div>`
     }</label>
     </div>`;
   };
 }
 
-async function appendField(
+function appendField(
   field: FormilaOptsFieldset['field']
 ) {
-  try {
-    return (
-      await Promise.all(field!.map((n) => {
-        const {
-          isPrefixed,
-          ...restN
-        } = n || {} as FormilaOptsFieldsetField;
+  return field!.map((n) => {
+    const {
+      isPrefixed,
+      ...restN
+    } = n || {} as FormilaOptsFieldsetField;
 
-        return appendLabel(n.isPrefixed!)(
-          restN as Omit<FormilaOptsFieldsetField, 'isPrefiexed'>
-        );
-      }))
-    )
-      .join('\n');
-  } catch (e) {
-    throw e;
-  }
+    return appendLabel(n.isPrefixed!)(
+      restN as Omit<FormilaOptsFieldsetField, 'isPrefiexed'>
+    );
+  }).join('\n');
 }
 
-async function appendFieldset(
+function appendFieldset(
   fieldset: FormilaOpts['fieldset']
 ) {
-  try {
-    return Array.isArray(fieldset) && fieldset.length > 0
-      ? (
-        await Promise.all(fieldset.map(async (n) => {
-          return html`<fieldset>
-            <!-- <h3>${n.title}</h3> -->
-            <legend>${n.title}</legend>
+  return Array.isArray(fieldset) && fieldset.length > 0
+    ? fieldset.map((n) => {
+      return `<fieldset>
+        <legend>${n.title}</legend>
 
-            <div class="fields-container">${
-              Array.isArray(n.field) && n.field.length > 0
-                ? appendField(n.field)
-                : ''
-            }</div>
-          </fieldset>`;
-        }))
-      )
-        .join('\n')
-      : '';
-  } catch (e) {
-    throw e;
-  }
+        <div class="fields-container">${
+          Array.isArray(n.field) && n.field.length > 0
+            ? appendField(n.field)
+            : ''
+        }</div>
+      </fieldset>`;
+    }).join('\n')
+    : '';
 }
 
-async function appendHidden(
+function appendHidden(
   hidden: FormilaOpts['hidden']
 ) {
   try {
     return Array.isArray(hidden) && hidden.length > 0
-      ? (
-        await Promise.all(hidden!.map(
-          async n => html`<input type="hidden" name="${n.name}" value="${n.value}"></input>`
-        ))
-      ).join('\n')
+      ? hidden!
+          .map(n => `<input type="hidden" name="${n.name}" value="${n.value}"></input>`)
+          .join('\n')
       : '';
   } catch (e) {
     throw e;
   }
 }
 
-export async function renderStyle() {
-  return html`<style>
-    /** [START] Reset element style */
-    button {
-      -webkit-appearance: none;
-      box-sizing: border-box;
+export function renderStyleSync() {
+  return serializeParsedFragment(`<style>
+  /** [START] Reset element style */
+  button {
+    -webkit-appearance: none;
+    box-sizing: border-box;
 
-      position: relative;
-      background-color: inherit;
-      color: inherit;
-      font-size: 14px;
-      border: none;
-    }
-    fieldset {
-      margin: 0;
-      padding: 0;
-      border: none;
-    }
-    fieldset > legend {
-      display: block;
-      font-size: 1.17em;
-      margin: 1em 0;
-      font-weight: 700;
-    }
-    /** [END] Reset element style */
+    position: relative;
+    background-color: inherit;
+    color: inherit;
+    font-size: 14px;
+    border: none;
+  }
+  fieldset {
+    margin: 0;
+    padding: 0;
+    border: none;
+  }
+  fieldset > legend {
+    display: block;
+    font-size: 1.17em;
+    margin: 1em 0;
+    font-weight: 700;
+  }
+  /** [END] Reset element style */
 
-    .label-container + .label-container {
-      margin: 10px 0 0;
-    }
+  .label-container + .label-container {
+    margin: 10px 0 0;
+  }
 
-    .prefixed-input {
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-    }
-    .prefixed-input.has-description {
-      flex-direction: column;
-      align-items: inherit;
-    }
-    label {
-      width: 100%;
-    }
-    label > input:not([type=radio]):not([type=checkbox]) {
-      width: 100%;
-    }
-    label > .prefixed-input > input:not([type=checkbox]) {
-      flex: 1 0 auto;
+  .prefixed-input {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+  .prefixed-input.has-description {
+    flex-direction: column;
+    align-items: inherit;
+  }
+  label {
+    width: 100%;
+  }
+  label > input:not([type=radio]):not([type=checkbox]) {
+    width: 100%;
+  }
+  label > .prefixed-input > input:not([type=checkbox]) {
+    flex: 1 0 auto;
 
-      width: 100%;
-      margin: 0 0 3px 0;
-    }
-    label > .prefixed-input > input[type="checkbox"] {
-      margin: 3px 8px 3px 3px;
-    }
-    label > input[aria-invalid=false],
-    label:not(.is-invalid) > .prefixed-input,
-    label.is-invalid > .prefixed-input + .error-msg,
-    label > input[aria-invalid=true] + .error-msg,
-    label > select {
-      margin: 0;
-    }
+    width: 100%;
+    margin: 0 0 3px 0;
+  }
+  label > .prefixed-input > input[type="checkbox"] {
+    margin: 3px 8px 3px 3px;
+  }
+  label > input[aria-invalid=false],
+  label:not(.is-invalid) > .prefixed-input,
+  label.is-invalid > .prefixed-input + .error-msg,
+  label > input[aria-invalid=true] + .error-msg,
+  label > select {
+    margin: 0;
+  }
 
-    label > .prefixed-input > .input-description {
-      font-size: .8em;
-      font-style: italic;
-      color: rgba(0, 0, 0, .75);
-      color: var(--input-description-color, rgba(0, 0, 0, .75));
-    }
+  label > .prefixed-input > .input-description {
+    font-size: .8em;
+    font-style: italic;
+    color: rgba(0, 0, 0, .75);
+    color: var(--input-description-color, rgba(0, 0, 0, .75));
+  }
 
-    .error-msg {
-      display: none;
-      color: #ff1744;
-      color: var(--error-msg-color, #ff1744);
-    }
+  .error-msg {
+    display: none;
+    color: #ff1744;
+    color: var(--error-msg-color, #ff1744);
+  }
 
-    label > input[aria-invalid=true],
-    label > input[aria-invalid=false] + .error-msg,
-    label.is-invalid > .prefixed-input,
-    label:not(.is-invalid) > .prefixed-input + .error-msg {
-      margin: 0;
-    }
+  label > input[aria-invalid=true],
+  label > input[aria-invalid=false] + .error-msg,
+  label.is-invalid > .prefixed-input,
+  label:not(.is-invalid) > .prefixed-input + .error-msg {
+    margin: 0;
+  }
 
-    label.is-invalid > .input-title {
-      color: #ff1744;
-      color: var(--error-color, #ff1744);
-    }
+  label.is-invalid > .input-title {
+    color: #ff1744;
+    color: var(--error-color, #ff1744);
+  }
 
-    input[aria-invalid=true] + .error-msg,
-    label.is-invalid > .prefixed-input + .error-msg {
-      display: block;
-    }
-    input[aria-invalid=true] {
-      border: 1px solid #ff1744;
-      border: 1px solid var(--error-color, #ff1744);
-    }
+  input[aria-invalid=true] + .error-msg,
+  label.is-invalid > .prefixed-input + .error-msg {
+    display: block;
+  }
+  input[aria-invalid=true] {
+    border: 1px solid #ff1744;
+    border: 1px solid var(--error-color, #ff1744);
+  }
 
-    .btn-container {
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      justify-content: center;
+  .btn-container {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
 
-      margin: 24px 40px 64px;
-    }
-    .btn-container > button[type=submit],
-    .btn-container > button[type=button] {
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      justify-content: center;
+    margin: 24px 40px 64px;
+  }
+  .btn-container > button[type=submit],
+  .btn-container > button[type=button] {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
 
-      position: relative;
-      margin: 0;
-      padding: 6px 16px;
-      background-color: rgba(0, 0, 0, 0);
-      background-color: var(--button-bg-color, rgba(0, 0, 0, 0));
-      color: #0070fb;
-      color: var(--button-color, #0070fb);
-      font-size: 14px;
-      border-radius: 2px;
-      text-transform: uppercase;
-    }
-    .btn-container > button[type=submit]:focus,
-    .btn-container > button[type=submit]:active,
-    .btn-container > button[type=button]:focus,
-    .btn-container > button[type=button]:active {
-      font-weight: 700;
-    }
-    .btn-container > button[type=submit]::after,
-    .btn-container > button[type=button]::after {
-      position: absolute;
-      content: '';
-      display: block;
-      width: 100%;
-      height: 100%;
-      top: 0;
-      left: 0;
-      background-color: rgba(0, 0, 0, 1);
-      background-color: var(--button-pseudoafter-bg-color, rgba(0, 0, 0, 1));
-      border-radius: inherit;
+    position: relative;
+    margin: 0;
+    padding: 6px 16px;
+    background-color: rgba(0, 0, 0, 0);
+    background-color: var(--button-bg-color, rgba(0, 0, 0, 0));
+    color: #0070fb;
+    color: var(--button-color, #0070fb);
+    font-size: 14px;
+    border-radius: 2px;
+    text-transform: uppercase;
+  }
+  .btn-container > button[type=submit]:focus,
+  .btn-container > button[type=submit]:active,
+  .btn-container > button[type=button]:focus,
+  .btn-container > button[type=button]:active {
+    font-weight: 700;
+  }
+  .btn-container > button[type=submit]::after,
+  .btn-container > button[type=button]::after {
+    position: absolute;
+    content: '';
+    display: block;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 1);
+    background-color: var(--button-pseudoafter-bg-color, rgba(0, 0, 0, 1));
+    border-radius: inherit;
 
-      opacity: 0;
-      transition: opacity 250ms cubic-bezier(0, 0, .4, 1);
-    }
-    .btn-container > button[type=submit]:focus::after,
-    .btn-container > button[type=submit]:active::after,
-    .btn-container > button[type=button]:focus::after,
-    .btn-container > button[type=button]:active::after {
-      opacity: .1;
-    }
-  </style>`;
+    opacity: 0;
+    transition: opacity 250ms cubic-bezier(0, 0, .4, 1);
+  }
+  .btn-container > button[type=submit]:focus::after,
+  .btn-container > button[type=submit]:active::after,
+  .btn-container > button[type=button]:focus::after,
+  .btn-container > button[type=button]:active::after {
+    opacity: .1;
+  }
+</style>`);
 }
 
-export async function renderHtml(data) {
+export function renderFormSync(opts) {
   const {
     title,
     subtitle,
@@ -327,28 +350,28 @@ export async function renderHtml(data) {
     hidden = [] as FormilaOpts['hidden'],
     fieldset = [] as FormilaOpts['fieldset'],
     submitTitle,
-  } = data;
+  } = opts;
 
-  return html`<form${
+  return serializeParsedFragment(`<form${
     attrTag == null
       ? ''
-      : ` ${await html`${attrTag}`}`
+      : ` ${attrTag}`
   }>
     ${
       title == null
         ? ''
-        : html`<h2 class="form__title">${title}</h2>`
+        : `<h2 class="form__title">${title}</h2>`
     }
     ${
       subtitle == null
         ? ''
-        : html`<p class="form__subtitle">${subtitle}</p>`
+        : `<p class="form__subtitle">${subtitle}</p>`
     }
 
     ${
       hidden == null
         ? ''
-        : html`<div class="form__hidden-input-container">${appendHidden(hidden)}</div>`
+        : `<div class="form__hidden-input-container">${appendHidden(hidden)}</div>`
     }
 
     ${appendFieldset(fieldset)}
@@ -360,14 +383,31 @@ export async function renderHtml(data) {
           : submitTitle
       }</button>
     </div>
-  </form>`;
+  </form>`);
+}
+
+export async function renderStyle() {
+  return renderStyleSync();
+}
+
+export async function renderForm(opts) {
+  return renderFormSync(opts);
+}
+
+export function formilaSync(
+  opts: Partial<FormilaOpts> = {} as FormilaOpts
+) {
+  return {
+    html: renderFormSync(opts),
+    style: renderStyleSync(),
+  };
 }
 
 export async function formila(
   opts: Partial<FormilaOpts> = {} as FormilaOpts
 ) {
   return {
-    html: await renderHtml(opts),
+    html: await renderForm(opts),
     style: await renderStyle(),
   };
 }
